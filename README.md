159d4570f140a30c81f3281fbc737370a6cc1cab
Philip Allen <philip.allen@searchdiscovery.com>
2013-12-09 14:54:03 -0500

    Modularized, reworked version

---
+++ README.md
@@ -0,0 +1,5 @@
+# Node.js Twitter Websocket Server
+
+A simple bootstrap app for live-streamed twitter applications on Node.js.
+
+Just run `npm install` to download dependencies, [create a new twitter app](https://dev.twitter.com/apps/new), input your authorization info into server.js, and fire it all up from the command line using `node server.js`. By default, the server will run at port 8080.
---
+++ server.js
@@ -0,0 +1,23 @@
+var WebsocketServer = require('ws-server'),
+    ws = new WebsocketServer('index.html'),
+
+    TwitterLink = require('twitter-link'),
+    TwitterStream = require('twitter-stream'),
+
+  stream = new TwitterStream({
+    auth : {
+      consumer_key : "...",
+      consumer_secret : "...",
+      access_token : "...",
+      access_token_secret : "..."
+    },
+    method : "track",
+    keywords : ['puppies', 'kittens'],
+    onResponse : ws.makeEmitter('tweet')
+  });
+
+// Start websocket server
+ws.start();
+
+// Connect to Twitter stream
+stream.connect();
